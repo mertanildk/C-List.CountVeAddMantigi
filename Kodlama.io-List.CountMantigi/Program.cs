35ec68f84e0384fd31988d991ce8c14269765370
MERT ANIL <107416952+mertanildk@users.noreply.github.com>
2022-06-27 15:59:43 +0300

    Kodlama.io

---
+++ Kodlama.io-List.CountMantigi/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+namespace Kodlama.io_List.CountMantigi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> list = new MyList<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            Console.WriteLine(list.Count);
+        }
+    }
+    class MyList<T>
+    {
+        T[] array;
+        T[] tempArray;
+        public MyList()
+        {
+            array = new T[0];
+        }
+        public void Add(T item)
+        {
+            tempArray = array;
+            array = new T[array.Length + 1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                array[i] = tempArray[i];
+            }
+            array[array.Length - 1] = item;
+
+        }
+        
+
+        public int Count
+        {
+            get { return array.Length; }
+           
+        }
+
+
+    }
+}
